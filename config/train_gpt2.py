33d84cb1a0fca111d2e41e87eb56a37f1046ed4c
hs-chae <hsc1403@snu.ac.kr>
2023-11-30 00:30:21 +0900

    new start

--- config/train_gpt2.py
+++ config/train_gpt2.py
@@ -3,23 +3,64 @@
 # $ torchrun --standalone --nproc_per_node=8 train.py config/train_gpt2.py
 
 wandb_log = True
-wandb_project = 'owt'
-wandb_run_name='gpt2-124M'
+wandb_project = 'layerwise distillation test'#'FineTune Investigation' #'AttentionTransfer3'
+wandb_run_name= 'small-prbl-weighted' #'gpt2-proj-block-12hd-weighted'#'only_qk_gpt2_MSE_combined' #'CRAMMED-GK-exp-12lyr-12-40-ddp'
 
 # these make the total batch size be ~0.5M
-# 12 batch size * 1024 block size * 5 gradaccum * 8 GPUs = 491,520
 batch_size = 12
 block_size = 1024
-gradient_accumulation_steps = 5 * 8
+gradient_accumulation_steps = 40
 
 # this makes total number of tokens be 300B
-max_iters = 600000
-lr_decay_iters = 600000
-
+max_iters = 100000
+lr_decay_iters = 100000
 # eval stuff
-eval_interval = 1000
-eval_iters = 200
+eval_interval = 25
+eval_iters = 2000
 log_interval = 10
 
 # weight decay
 weight_decay = 1e-1
+
+compile=False
+out_dir = 'TheFuture/small-prbl-weighted' #'config/GK-exp2poly-12lyrs-combined'
+
+dataset = 'openwebtext'
+
+bias=True
+init_from = 'manipulate' #'finetune_engineer' #'engineer'
+dropout = 0.0
+
+
+
+# #gpt-2 base
+n_layer = 12
+n_head = 12
+n_embd=768
+
+# 'gpt2':         dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+#             'gpt2-medium':  dict(n_layer=24, n_head=16, n_embd=1024), # 350M params
+#             'gpt2-large':   dict(n_layer=36, n_head=20, n_embd=1280), # 774M params
+#             'gpt2-xl':      dict(n_layer=48, n_head=25, n_embd=1600), # 1558M params
+
+# n_layer=24
+# n_head=16
+# n_embd=1024
+
+# n_layer=36
+# n_head=20
+# n_embd=1280
+
+# n_layer=48
+# n_head=25
+# n_embd=1600
+
+
+# init_from = 'scratch'
+# wandb_project = 'ft_TinyStories'
+# wandb_run_name= '1block_2to6_myGELU2' #'gpt2-proj-block-12hd-weighted'#'only_qk_gpt2_MSE_combined' #'CRAMMED-GK-exp-12lyr-12-40-ddp
+# n_layer = 1
+# n_head = 1
+# n_embd=768
+# out_dir = 'attn_transfer/1block-2to6-myGELU2' #'config/GK-exp2poly-12lyrs-combined
+# dataset= 'TinyStories'
--- model.py
+++ model.py
@@ -15,13 +15,30 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+
+
+def subtraction_gaussian_kernel_torch(q, k):
+    device = q.device
+    k = k.transpose(-1, -2)
+    #print(f'max k = {torch.max(k)}, max q = {torch.max(q)}')
+    matA_square = q ** 2. @ torch.ones(k.shape[-2:]).to(device)
+    matB_square = torch.ones(q.shape[-2:]).to(device) @ k ** 2.
+    return matA_square + matB_square - 2. * (q @ k)
+
+
+def myGELU(x): #GeLU Taylor approx
+    return 0.5*x+0.398942*(x**2)-0.0668195*(x**4) 
+
+
+
 class LayerNorm(nn.Module):
     """ LayerNorm but with an optional bias. PyTorch doesn't support simply bias=False """
 
     def __init__(self, ndim, bias):
         super().__init__()
+        #print(f"Layernorm BIAS: = {bias}")
         self.weight = nn.Parameter(torch.ones(ndim))
-        self.bias = nn.Parameter(torch.zeros(ndim)) if bias else None
+        self.bias = nn.Parameter(torch.zeros(ndim)) #if bias else None
 
     def forward(self, input):
         return F.layer_norm(input, self.weight.shape, self.weight, self.bias, 1e-5)
@@ -32,6 +49,9 @@ class CausalSelfAttention(nn.Module):
         super().__init__()
         assert config.n_embd % config.n_head == 0
         # key, query, value projections for all heads, but in a batch
+
+        print(f"ATTN BIAS :  {config.bias}")
+        print(f"ATTN DROPOUT : {config.dropout} ")
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
@@ -42,14 +62,16 @@ class CausalSelfAttention(nn.Module):
         self.n_embd = config.n_embd
         self.dropout = config.dropout
         # flash attention make GPU go brrrrr but support is only in PyTorch >= 2.0
-        self.flash = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+        self.flash = False #hasattr(torch.nn.functional, 'scaled_dot_product_attention')
         if not self.flash:
-            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+            print("WARNI"
+                  "NG: using slow attention. Flash Attention requires PyTorch >= 2.0")
             # causal mask to ensure that attention is only applied to the left in the input sequence
             self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
                                         .view(1, 1, config.block_size, config.block_size))
+        #self.counter = 0
 
-    def forward(self, x):
+    def forward(self, x, get_qk=False):
         B, T, C = x.size() # batch size, sequence length, embedding dimensionality (n_embd)
 
         # calculate query, key, values for all heads in batch and move head forward to be the batch dim
@@ -58,18 +80,56 @@ class CausalSelfAttention(nn.Module):
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) # (B, nh, T, hs)
 
+        
+        
+        self.flash = False  #Set True for flash-attention
+        use_softmax = False #Set True for softmax, false for Gaussian Kernel
+        use_approx = False #Set True to use torch.exp, false to use polynomial approx
+        return_qkdiff = get_qk
+
         # causal self-attention; Self-attend: (B, nh, T, hs) x (B, nh, hs, T) -> (B, nh, T, T)
         if self.flash:
+            print("FLASH")
             # efficient attention using Flash Attention CUDA kernels
             y = torch.nn.functional.scaled_dot_product_attention(q, k, v, attn_mask=None, dropout_p=self.dropout if self.training else 0, is_causal=True)
-        else:
+
+        elif use_softmax:
+            
             # manual implementation of attention
             att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
             att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
             att = F.softmax(att, dim=-1)
             att = self.attn_dropout(att)
             y = att @ v # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
-        y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+            y = y.transpose(1, 2).contiguous().view(B, T, C) # re-assemble all head outputs side by side
+
+        else:
+            #Gaussian Kernel
+            qk_diff = subtraction_gaussian_kernel_torch(q, k)
+            self.norm_factor = torch.tensor(1.0 / math.sqrt(k.size(-1))).to(q.device)
+            qk_diff *= -self.norm_factor * 0.5
+
+            if use_approx:
+            # exp approx mode
+                att = (lambda x: (1 + x / 2 ** 8) ** (2 ** 8))(qk_diff)
+                att = att.masked_fill(self.bias[:, :, :T, :T] == 0, 0)
+
+            # exp mode
+            else :
+                qk_diff = qk_diff.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+                att = torch.exp(qk_diff)
+
+
+                mask = qk_diff != float('-inf')
+                masked_tensor = torch.masked_select(qk_diff, mask)
+                max_val = torch.max(masked_tensor)
+                min_val = torch.min(masked_tensor)
+                #print(f"Max, min : {max_val}, {min_val}")
+
+            
+            att = self.attn_dropout(att)
+            y = att @ v
+            y = y.transpose(1, 2).contiguous().view(B, T, C)
 
         # output projection
         y = self.resid_dropout(self.c_proj(y))
@@ -79,14 +139,17 @@ class MLP(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        print(f"MLP BIAS: = {config.bias}")
         self.c_fc    = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
         self.gelu    = nn.GELU()
+        self.relu = nn.ReLU()
         self.c_proj  = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
         self.dropout = nn.Dropout(config.dropout)
+        self.mygelu = myGELU
 
     def forward(self, x):
         x = self.c_fc(x)
-        x = self.gelu(x)
+        x = self.gelu(x) #myGELU(x) # self.gelu(x)
         x = self.c_proj(x)
         x = self.dropout(x)
         return x
@@ -95,15 +158,26 @@ class Block(nn.Module):
 
     def __init__(self, config):
         super().__init__()
+        print(f"BLOCK BIAS: = {config.bias}")
         self.ln_1 = LayerNorm(config.n_embd, bias=config.bias)
         self.attn = CausalSelfAttention(config)
         self.ln_2 = LayerNorm(config.n_embd, bias=config.bias)
         self.mlp = MLP(config)
+        #self.counter = 0
 
     def forward(self, x):
+        get_attn = False
+        get_qk = False
+        if get_qk:
+            y, qk =  self.attn(self.ln_1(x),get_qk)
+            x = x + y
+            x = x + self.mlp(self.ln_2(x))
+            return x, qk
         x = x + self.attn(self.ln_1(x))
         x = x + self.mlp(self.ln_2(x))
         return x
+
+
 
 @dataclass
 class GPTConfig:
@@ -113,7 +187,7 @@ class GPTConfig:
     n_head: int = 12
     n_embd: int = 768
     dropout: float = 0.0
-    bias: bool = True # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
+    bias: bool = False # True: bias in Linears and LayerNorms, like GPT-2. False: a bit better and faster
 
 class GPT(nn.Module):
 
@@ -122,6 +196,7 @@ class GPT(nn.Module):
         assert config.vocab_size is not None
         assert config.block_size is not None
         self.config = config
+        print(f"GPT BIAS: = {config.bias}")
 
         self.transformer = nn.ModuleDict(dict(
             wte = nn.Embedding(config.vocab_size, config.n_embd),
@@ -146,6 +221,9 @@ class GPT(nn.Module):
 
         # report number of parameters
         print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
+        #self.counter = 0
+        #print("GPT INITIALIZATION IS FINE")
+
 
     def get_num_params(self, non_embedding=True):
         """
@@ -167,7 +245,8 @@ class GPT(nn.Module):
         elif isinstance(module, nn.Embedding):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
-    def forward(self, idx, targets=None):
+    def forward(self, idx, targets=None, get_attn = False):
+        #get_attn = False
         device = idx.device
         b, t = idx.size()
         assert t <= self.config.block_size, f"Cannot forward sequence of length {t}, block size is only {self.config.block_size}"
@@ -177,20 +256,36 @@ class GPT(nn.Module):
         tok_emb = self.transformer.wte(idx) # token embeddings of shape (b, t, n_embd)
         pos_emb = self.transformer.wpe(pos) # position embeddings of shape (t, n_embd)
         x = self.transformer.drop(tok_emb + pos_emb)
+
+        #print("TRANSFORMERS START")
+        #qount = 0
+        get_qk = False
         for block in self.transformer.h:
-            x = block(x)
+            #block.count(self.counter)
+            if get_qk : 
+                x, qk = block(x)
+            else: 
+                x = block(x)
+            #print(f"OK WITH TRANSFORMER : {qount}")
+            #qount+=1
         x = self.transformer.ln_f(x)
+
 
         if targets is not None:
             # if we are given some desired targets also calculate the loss
             logits = self.lm_head(x)
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+            print(loss)
         else:
             # inference-time mini-optimization: only forward the lm_head on the very last position
             logits = self.lm_head(x[:, [-1], :]) # note: using list [-1] to preserve the time dim
             loss = None
 
-        return logits, loss
+        if get_attn:
+            return logits, loss, attn_weights_list
+        else:
+            return logits, loss
+
 
     def crop_block_size(self, block_size):
         # model surgery to decrease the block size if necessary
@@ -303,17 +398,22 @@ class GPT(nn.Module):
         return mfu
 
     @torch.no_grad()
-    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None):
+    def generate(self, idx, max_new_tokens, temperature=1.0, top_k=None, get_attn=False):
         """
         Take a conditioning sequence of indices idx (LongTensor of shape (b,t)) and complete
         the sequence max_new_tokens times, feeding the predictions back into the model each time.
         Most likely you'll want to make sure to be in model.eval() mode of operation for this.
         """
+        
+
         for _ in range(max_new_tokens):
             # if the sequence context is growing too long we must crop it at block_size
             idx_cond = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
             # forward the model to get the logits for the index in the sequence
-            logits, _ = self(idx_cond)
+            if get_attn:
+                logits, _, attn = self(idx_cond, get_attn=get_attn)
+            else:
+                logits, _ = self(idx_cond)
             # pluck the logits at the final step and scale by desired temperature
             logits = logits[:, -1, :] / temperature
             # optionally crop the logits to only the top k options
@@ -328,3 +428,6 @@ class GPT(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1)
 
         return idx
+
+    # def count(self, iter_num):
+    #     self.counter = iter_num
